dc77d237bec700c899c641011d5f528b71804959
fatima Boet <fatimaboet@gmail.com>
2023-01-25 18:44:27 -0400

    cambios front landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -97,6 +97,7 @@ export class LandingComponent implements OnInit {
   }
 
   open(content, type, modalDimension) {
+    
     if (modalDimension === 'sm' && type === 'modal_mini') {
         this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
             this.closeResult = `Closed with: ${result}`;
@@ -109,7 +110,13 @@ export class LandingComponent implements OnInit {
       }, (reason) => {
           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
-    } else {
+    } else if (modalDimension === '' && type === 'Cotizar') {
+    this.modalService.open(content, { windowClass: 'modal-cotizar', centered: true }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  } else {
         this.modalService.open(content,{ centered: true }).result.then((result) => {
             this.closeResult = `Closed with: ${result}`;
         }, (reason) => {
