9c1fe8a47547792070b24215d729ada60071119c
fatima Boet <fatimaboet@gmail.com>
2023-03-10 17:21:16 -0400

    cambios f

--- src/app/pedido-historial-ecommerce/pedido-historial-ecommerce.component.ts
+++ src/app/pedido-historial-ecommerce/pedido-historial-ecommerce.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons,  NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pedido-historial-ecommerce',
   templateUrl: './pedido-historial-ecommerce.component.html',
-  styleUrls: ['./pedido-historial-ecommerce.component.css']
+  styleUrls: ['./pedido-historial-ecommerce.component.scss']
 })
 export class PedidoHistorialEcommerceComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string;
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  open(content, type) {
+    console.log(type)
+    if(type == 'detail'){
+      this.modalService.open(content, { windowClass: 'modal-detail', centered: true, backdrop: false }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {      
+      });
+    } 
   }
 
 }
