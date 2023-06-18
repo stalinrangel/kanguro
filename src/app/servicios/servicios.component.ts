10969010fce98a18a3fccd06e6914f201a0f27f8
Fatima Paola Boet Abreu <fatimapaolaboetabreu@MacBook-Air-de-Fatima.local>
2023-06-18 01:44:40 -0400

    page servicios

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  styleUrls: ['./servicios.component.scss']
 })
 export class ServiciosComponent implements OnInit {
 
-  constructor() { }
+  showWeb: boolean = false;
+
+  constructor() { 
+    var mediaqueryList = window.matchMedia("(min-width: 992px)");
+    if(mediaqueryList.matches) {
+      this.showWeb = true;
+    }
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
