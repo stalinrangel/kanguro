6ba954ce06715b8f575c3fde38e79449c9bc5f9a
Fatima Paola Boet Abreu <fatimapaolaboetabreu@MacBook-Air-de-Fatima.local>
2023-07-03 09:09:16 -0400

    seccion contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent implements OnInit {
+  showWeb: boolean = false;
 
-  constructor() { }
-
+  constructor() { 
+    var mediaqueryList = window.matchMedia("(min-width: 992px)");
+    if(mediaqueryList.matches) {
+      this.showWeb = true;
+    }
+  }
   ngOnInit(): void {
   }
 
